60d2f8993f733b8837b665cd7e777d5b84efd58c
Nikolas Stonys <s067029@ad.viko.lt>
2026-09-17 11:13:06 +0300

    duomenu tipu pavyzdys

---
+++ duomenu-tipai/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string vardas = "Nikolas";
+    string pavarde = "Stonys";
+    int amzius = 19;
+    string grupe = "PI26";
+    int kursas = 1;
+    string programosPav = "Programu sistemos";
+    cout << "Pirmoji C++ programa";
+    return 0;
+}
