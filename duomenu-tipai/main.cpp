7c6ffa2bad5ff2d60a78b32ab91459cc003c22b0
Nikolas Stonys <s067029@ad.viko.lt>
2026-09-24 11:43:13 +0300

    1, 2, 3, 4 uzduotys atliktos

--- duomenu-tipai/main.cpp
+++ duomenu-tipai/main.cpp
@@ -103,23 +103,96 @@ int main() {
 
 
     // Studento pazymiu statistika
-    const int studentGradesNum = 5;
-    int grade;
-    int sum = 0;
-    int highestGrade = 0;
+    // const int studentGradesNum = 5;
+    // int grade;
+    // int sum = 0;
+    // int highestGrade = 0;
+    //
+    // for (int i = 1; i<= studentGradesNum; i++) {
+    //     cout << "Iveskite "<<i<<" studento pazymi."<<endl;
+    //     cin >> grade;
+    //     sum += grade;
+    //
+    //     highestGrade = (grade > highestGrade) ? grade : highestGrade;
+    // }
+    //
+    // double averageGrade = static_cast<double>(sum) / studentGradesNum;
+    // cout <<fixed << setprecision(2) <<"Pazymiu vidurkis " << averageGrade << endl;
+    // cout << "Didziausias pazymys "<< highestGrade << endl;
 
-    for (int i = 1; i<= studentGradesNum; i++) {
-        cout << "Iveskite "<<i<<" studento pazymi."<<endl;
-        cin >> grade;
-        sum += grade;
+    //Uzduotys 1, 2, 3, 4
 
-        highestGrade = (grade > highestGrade) ? grade : highestGrade;
-    }
+    // int grade1;
+    // int grade2;
+    // int grade3;
+    // int grade4;
+    // int grade5;
+    // int suma = 0;
+    //
+    // cout << "Iveskite pirma pazymi" <<endl;
+    // cin >> grade1;
+    // cout << "Iveskite antra pazymi" <<endl;
+    // cin >> grade2;
+    // cout << "Iveskite trecia pazymi" <<endl;
+    // cin >> grade3;
+    // cout << "Iveskite ketvirta pazymi" <<endl;
+    // cin >> grade4;
+    // cout << "Iveskite penkta pazymi" <<endl;
+    // cin >> grade5;
+    //
+    // double vidurkis = static_cast<double>(grade1 + grade2 + grade3 + grade4 + grade5) / 5;
+    // cout << "Vidurkis yra " << vidurkis << endl;
+    // if (vidurkis > 5) {
+    //     cout << "Vidurkis teigiamas. " << endl;
+    // } else {
+    //     cout << "Vidurkis yra neigiamas. " << endl;
+    // }
+    //
+    // int egzaminoGrade;
+    // cout << "Iveskite egzamino ivertinima. " << endl;
+    // cin >> egzaminoGrade;
 
-    double averageGrade = static_cast<double>(sum) / studentGradesNum;
-    cout <<fixed << setprecision(2) <<"Pazymiu vidurkis" << averageGrade << endl;
-    cout << "Didziausias pazymys "<< highestGrade << endl;
+    //Per if else (3 uzd)
 
+    // if (egzaminoGrade == 10) {
+    //     cout << "Egzaminas islaikytas puikiai!" << endl;
+    // } else if (egzaminoGrade >= 9) {
+    //     cout << "Egzaminas islaikytas labai gerai!" << endl;
+    // } else if (egzaminoGrade >= 7) {
+    //     cout << "Egzaminas islaikytas gerai!" << endl;
+    // } else if (egzaminoGrade >= 5) {
+    //     cout << "Egzaminas islaikytas patenkinamai" << endl;
+    // } else if (egzaminoGrade < 5) {
+    //     cout << "Egzaminas neislaikytas" << endl;
+    // }
+
+    //Per switch (4uzd)
+
+    // switch (egzaminoGrade) {
+    //     case 10:
+    //         cout << "Egzaminas islaikytas puikiai!" << endl;
+    //         break;
+    //     case 9:
+    //         cout << "Egzaminas islaikytas labai gerai!" << endl;
+    //         break;
+    //     case 8:
+    //     case 7:
+    //         cout << "Egzaminas islaikytas gerai!" << endl;
+    //         break;
+    //     case 6:
+    //     case 5:
+    //         cout << "Egzaminas islaikytas patenkinamai" << endl;
+    //         break;
+    //     default:
+    //         cout << "Egzaminas neislaikytas" << endl;
+    //
+    // }
+
+
+
+    // 5 uzduotis
+
+    
 
     return 0;
 }
